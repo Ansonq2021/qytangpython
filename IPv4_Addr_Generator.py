05921392a240e632fa889a0d443b911b07e0215a
anson.qin <evianq@gmail.com>
2022-06-27 15:56:38 -0400

    Create README.md

--- IPv4_Addr_Generator.py
+++ IPv4_Addr_Generator.py
@@ -1,4 +1,4 @@
 import random
 # Generate randomly IPv4 address
-Random_IPv4_Addr = str(random(0, 255)) + '.' + str(random(0, 255)) + '.' + str(random(0, 255)) + '.' + str(random(0, 255))
+Random_IPv4_Addr = str(random.randint(0, 255)) + '.' + str(random.randint(0, 255)) + '.' + str(random.randint(0, 255)) + '.' + str(random.randint(0, 255))
 print (Random_IPv4_Addr)
