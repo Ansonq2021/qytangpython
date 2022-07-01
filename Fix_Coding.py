16fa6e6f858bae88b7373f3ac35e224085830819
anson.qin <evianq@gmail.com>
2022-06-30 21:47:01 -0400

    Create README.md

--- Fix_Coding.py
+++ Fix_Coding.py
@@ -8,6 +8,9 @@ COURSE_FEES_PYTHON = 1234.3456
 line1 = 'Department1 name:%-16s Manager:%-16s COURSE FEES:%-12.5f %16s' % ('Security', 'cq_bomb', 456789.12456, 'The END!')
 line2 = 'Department2 name:%-16s Manager:%-16s COURSE FEES:%-9.4f %19s' % ('Python', 'qinke', 1234.3456, 'The END!')
 
+line1 = f'Department1: {department1:<16} Manager: {depart1_m:<16} COURSE FEES: {COURSE_FEES_SEC:12.5f}' + '{:>16}'.format('The END!')
+line2 = f'Department2: {department2:<16} Manager: {depart2_m:<16} COURSE FEES: {COURSE_FEES_PYTHON:9.4f}' + '{:>19}'.format('The END!')
+
 
 length = len(line1)
 print ('='*length)
---
+++ RE_Match_IP_Address.py
@@ -0,0 +1,1 @@
+str1 = 'Port-channel1.189     192.168.189.254    YES   CONFIG   up'
