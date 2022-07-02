298945f1879e238b0e510dc50f8a9f22c5d7db72
anson.qin <evianq@gmail.com>
2022-07-02 00:39:20 -0400

    Create README.md

---
+++ RE_IP.py
@@ -0,0 +1,8 @@
+import re
+str1 = 'Port-channel1.189     192.168.189.254  YES   CONFIG  up         up   '
+show_interface = re.match('(\w+\-\w+\.\d+)\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+(\w+)\s+(\w+)\s+(\w+)\s+(\w+)\s',str1).groups()
+# Get requested parameters from interface
+print('-'*120)
+print('{:<15}'.format('Interface') + '{:<3}'.format(':') + f'{show_interface[0]:<20}')
+print('{:<15}'.format('IP Address') + '{:<3}'.format(':') + f'{show_interface[1]:<20}')
+print('{:<15}'.format('Status') + '{:<3}'.format(':') + f'{show_interface[5]:<20}')
