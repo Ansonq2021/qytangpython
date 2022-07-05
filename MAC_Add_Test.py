6e8b17623443be34c3715d10695745d402994f39
anson.qin <evianq@gmail.com>
2022-07-05 18:36:48 -0400

    Create README.md

--- MAC_Add_Test.py
+++ MAC_Add_Test.py
@@ -1,6 +1,6 @@
 import re
 MAC_Addr = '166 54a2.74f7.0326 DYNAMIC Gi1/0/11'
-show_mac_addr = re.match('(\d{1,4})\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w\w+[\d/\/]{5})', MAC_Addr).groups()
+show_mac_addr = re.match('(\d{1,4})\s+([0-9a-f]{4}\.[0-9a-f]{4}\.[0-9a-f]{4})\s+(\w+)\s+(\w\w+[\d/\/]{8})', MAC_Addr).groups()
 print(show_mac_addr)
 print('{:<12}'.format('VLAN ID') + '{:<3}'.format(':') + f'{show_mac_addr[0]:<20}')
 print('{:<12}'.format('MAC') + '{:<3}'.format(':') + f'{show_mac_addr[1]:<20}')
