ab57a2954e0dadf36aeeadebd9679d4e12769a16
anson.qin <evianq@gmail.com>
2022-07-05 23:18:58 -0400

    Create README.md

--- ASA_show_Conn.py
+++ ASA_show_Conn.py
@@ -1,7 +1,7 @@
 import re
 ASA_Show_Conn = 'TCP server 172.16.1.101:443 localserver 172.16.66.1:53710, idle 0:01:09, bytes 27575949, flags UIO'
 ASA_Conn_Para = re.split('[\s\,]', ASA_Show_Conn)
-print(ASA_Show_Conn)
+
 print('{:<12}'.format('Protocol') + '{:^3}'.format(':') + f'{ASA_Conn_Para[0]:<15}')
 print(f'{ASA_Conn_Para[1]:<12}' + '{:^3}'.format(':') + f'{ASA_Conn_Para[2]:<15}')
 print(f'{ASA_Conn_Para[3]:<12}' + '{:^3}'.format(':') + f'{ASA_Conn_Para[4]:<20}')
@@ -10,6 +10,3 @@ Idle_Time = str(f'{Time_Reformat[0]}' + ' Hour ' + f'{Time_Reformat[1]}' + ' Min
 print(f'{ASA_Conn_Para[6]:<12}' + '{:^3}'.format(':') + f'{Idle_Time:<15}')
 print(f'{ASA_Conn_Para[9]:<12}' + '{:^3}'.format(':') + f'{ASA_Conn_Para[10]:<15}')
 print(f'{ASA_Conn_Para[12]:<12}' + '{:^3}'.format(':') + f'{ASA_Conn_Para[13]:<15}')
-
-
-
